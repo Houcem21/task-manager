bdea3d8520aaa9b85ab1a5bde6d9fe728c25f84b
houcem21 <damakhoucem90@gmail.com>
2025-06-25 14:37:28 +0900

    looks

--- new-task.php
+++ new-task.php
@@ -5,16 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="index.css" rel="stylesheet" />
+    <link href="output.css" rel="stylesheet">
 </head>
 <body>
-    <form action="./api/create-task.php" method="post">
+    <form action="./api/create-task.php" method="post" class="w-[100vw] h-[100vh] flex flex-col gap-5 justify-center text-center items-center">
         <label for="title">
             Title
+            <br />
             <input required type="text" id="title" name="title" style="margin-bottom: 20px;" />
         </label>
-        <br />
         <label for="description">
             Description
+            <br />
             <textarea id="description" name="description" rows="5" cols="20" ></textarea>
         </label>
 
